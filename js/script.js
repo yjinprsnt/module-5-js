a4435bd42d848fa15ded6064cf2a8b0d8e72fce2
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-03-02 20:19:23 +0200

    add 4 auto check js module-5

--- js/script.js
+++ js/script.js
@@ -1985,3 +1985,17 @@
 //   },
 //   // Change code above this line
 // };
+
+const parent = {
+  name: 'Stacey',
+  surname: 'Moore',
+  age: 54,
+  heritage: 'Irish',
+};
+// Change code below this line
+
+const child = Object.create(parent);
+
+// Change code above this line
+child.name = 'Jason';
+child.age = 27;
