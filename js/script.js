fcaa519dcea23f4fcf4842beac49a3e18a26cdb2
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-03-03 16:25:53 +0200

    add 10 auto check js module-5

--- js/script.js
+++ js/script.js
@@ -2057,3 +2057,29 @@
 
 //   // Change code above this line
 // }
+
+// class Storage {
+//   constructor(items) {
+//     this.items = items;
+//   }
+//   getItems() {
+//     return this.items;
+//   }
+//   addItem(newItem) {
+//     this.items.push(newItem);
+//   }
+//   removeItem(itemToRemove) {
+//     const idx = this.items.indexOf(itemToRemove);
+//     if (idx !== -1) {
+//       this.items.splice(idx, 1);
+//     }
+//   }
+// }
+
+// // Change code above this line
+// const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem('Droid');
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem('Prolonger');
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
