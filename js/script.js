21c754c6a564fbb179d04f11de1a3d3c5dedef51
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-03-05 22:51:06 +0200

    add 17 auto check js module-5

--- js/script.js
+++ js/script.js
@@ -2264,3 +2264,25 @@
 
 // audi.price = 51000;
 // console.log(audi.price); // 49000
+
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   static checkPrice(price) {
+//     if (price > this.#MAX_PRICE) {
+//       return 'Error! Price exceeds the maximum';
+//     }
+//     return 'Success! Price is within acceptable limits';
+//   }
+//   // Change code below this line
+
+//   // Change code above this line
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
