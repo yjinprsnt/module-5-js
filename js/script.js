fa6d7de997c8e4632edfd91d299b39044168d7bc
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-03-05 21:33:03 +0200

    add 12 auto check js module-5

--- js/script.js
+++ js/script.js
@@ -2111,3 +2111,23 @@
 // console.log(builder.getValue()); // "^.^"
 // builder.padBoth('=');
 // console.log(builder.getValue()); // "=^.^="
+
+// class Car {
+//   // Change code below this line
+//   #brand;
+
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+
+//   getBrand() {
+//     return this.#brand;
+//   }
+
+//   changeBrand(newBrand) {
+//     return (this.#brand = newBrand);
+//   }
+//   // Change code above this line
+// }
