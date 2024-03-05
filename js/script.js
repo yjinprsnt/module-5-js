2912ab172b6c7740021f4bef604b9d42ab70c284
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-03-05 22:38:03 +0200

    add 16 auto check js module-5

--- js/script.js
+++ js/script.js
@@ -2234,3 +2234,33 @@
 //   }
 //   // Change code above this line
 // }
+
+// class Car {
+//   // Change code below this line
+//   #price;
+//   static MAX_PRICE = 50000;
+
+//   constructor({ price }) {
+//     this.#price = price;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     if (newPrice <= Car.MAX_PRICE) {
+//       this.#price = newPrice;
+//     }
+//   }
+//   // Change code above this line
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
