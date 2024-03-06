0f6a0121cd2ea37bf11027a9a5b266d656d32633
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-03-06 21:07:17 +0200

    add 19 auto check js module-5

--- js/script.js
+++ js/script.js
@@ -2304,3 +2304,42 @@
 // class Admin extends User {
 //   static AccessLevel = { BASIC: 'basic', SUPERUSER: 'superuser' };
 // }
+
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   // Change code below this line
+
+//   static AccessLevel = {
+//     BASIC: 'basic',
+//     SUPERUSER: 'superuser',
+//   };
+//   constructor({ email, accessLevel }) {
+//     super(email);
+//     this.accessLevel = Admin.AccessLevel;
+//   }
+
+//   // Change code above this line
+// }
+
+// const mango = new Admin({
+//   email: 'mango@mail.com',
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
